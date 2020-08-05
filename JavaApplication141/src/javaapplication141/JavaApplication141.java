c5ec6e0aab65e40c38923d67c7532db73853d8b1
MiquelVidalCortes <miquelvidalcortes@gmail.com>
2020-08-05 20:37:35 +0200

    segona prova netbeans

--- JavaApplication141/src/javaapplication141/JavaApplication141.java
+++ JavaApplication141/src/javaapplication141/JavaApplication141.java
@@ -6,6 +6,7 @@ public class JavaApplication141 {
     public static void main(String[] args) {
         System.out.println("MEMORIES");
         System.out.println("lol");
+        System.out.println("PERRY");
     }
     
 }
