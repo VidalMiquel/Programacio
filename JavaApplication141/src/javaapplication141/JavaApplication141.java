7d98f709c3f0d1da46e87f37c32ade5c9b6eea85
MiquelVidalCortes <miquelvidalcortes@gmail.com>
2020-08-05 20:36:05 +0200

    primera prova netbeans amb git

---
+++ JavaApplication141/src/javaapplication141/JavaApplication141.java
@@ -0,0 +1,11 @@
+
+package javaapplication141;
+
+public class JavaApplication141 {
+
+    public static void main(String[] args) {
+        System.out.println("MEMORIES");
+        System.out.println("lol");
+    }
+    
+}
